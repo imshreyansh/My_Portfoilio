f88e3504767188c5c4e34e513282b51f45c5e6ab
Shreyansh Upadhyay <shreyu.upadhyay13@gmail.com>
2019-08-21 18:35:21 +0530

    added icons

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './HomePage.css'
 import img from './img.jpg'
+import todo from './todo.png'
+import me from './me.png'
+import peeps from './peeps.png'
 class HomePage extends Component {
     render() {
         return (
@@ -9,6 +12,24 @@ class HomePage extends Component {
                     <div className='img'>
                         <img className='myImg' src={img} alt="" />
                     </div>
+                    <div className='nameHead'>
+                        <span className='nameText'>SHREYANSH UPADHYAY</span>
+                    </div>
+                    <div className='nameHeadTwo'>
+                        <span className='positionText'>Full Stack Developer</span>
+                    </div>
+                    <div className='details'>
+                        <span className='motivation'>Motivation:</span>
+                    </div>
+                    <div className='details'>
+                        <span className='detailsText'>Do not wait; the time will never be 'just right.' Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.</span>
+                    </div>
+                    <div className='ico'>
+                        <img className='myImg' src={todo} alt="" />
+                        <img className='myImg' src={me} alt="" />
+                        <img className='myImg' src={peeps} alt="" />
+                    </div>
+
                 </div>
             </div>
         )
