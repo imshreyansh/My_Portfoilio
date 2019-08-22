02325e5cebb374af140b3a02767fc02a7d5c728b
Shreyansh Upadhyay <shreyu.upadhyay13@gmail.com>
2019-08-22 12:31:29 +0530

    updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Todo from './Todo'
 import HomePage from './HomePage'
 import logo from './logo.png'
+import fb from './facebook.png'
+import ln from './linkedin.png'
+import In from './instagram.png'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <div>
-        <div className='logo'>
-          <img className='logoImg' src={logo} alt="" />
+        <div className='both'>
+          <div className='logo'>
+            <img className='logoImg' src={logo} alt="" />
+          </div>
+          <div className='contact'>
+            <a href="https://www.facebook.com/shreyansh.upadhyay02"><img className='ln' src={fb} alt="" /></a>
+            <a href="https://in.linkedin.com/in/imshreyansh"><img className='fb' src={ln} alt="" /></a>
+            <a href="https://www.instagram.com/imshreyansh_sk/"><img className='in' src={In} alt="" /></a>
+          </div>
         </div>
         <Router>
           <Route path='/todo' exact component={Todo} />
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -25,9 +25,18 @@ class HomePage extends Component {
                         <span className='detailsText'>Do not wait; the time will never be 'just right.' Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.</span>
                     </div>
                     <div className='ico'>
-                        <img className='myImgOne' src={todo} alt="" />
-                        <img className='myImgOne' src={me} alt="" />
-                        <img className='myImgOne' src={peeps} alt="" />
+                        <div className='spec'>
+                            <img className='myImgOne' src={todo} alt="" />
+                            <span className='di'>Projects</span>
+                        </div>
+                        <div className='spec'>
+                            <img className='myImgOne' src={me} alt="" />
+                            <span className='di'>About Me !</span>
+                        </div>
+                        <div className='spec'>
+                            <img className='myImgOne' src={peeps} alt="" />
+                            <span className='di'>Collections</span>
+                        </div>
                     </div>
 
                 </div>
