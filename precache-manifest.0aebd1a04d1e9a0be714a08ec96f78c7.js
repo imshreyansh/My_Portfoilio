1cb67d5ac75e27369a7e16f65151e2654bdf6447
Shreyansh Upadhyay <shreyu.upadhyay13@gmail.com>
2019-08-22 22:38:42 +0530

    Updates

---
+++ precache-manifest.0aebd1a04d1e9a0be714a08ec96f78c7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91dac7a5274b068deba3a2d3849a206c",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "6e608aa2d3862e6781f3",
+    "url": "/My_Portfolio/static/css/main.edb69008.chunk.css"
+  },
+  {
+    "revision": "3da17d12fbc508edcb60",
+    "url": "/My_Portfolio/static/js/2.acb14d8f.chunk.js"
+  },
+  {
+    "revision": "6e608aa2d3862e6781f3",
+    "url": "/My_Portfolio/static/js/main.9d833760.chunk.js"
+  },
+  {
+    "revision": "82fe216a321529713cbb",
+    "url": "/My_Portfolio/static/js/runtime~main.0406a893.js"
+  },
+  {
+    "revision": "e4941a7adaa0d24e1503c3c70c88e095",
+    "url": "/My_Portfolio/static/media/bg.e4941a7a.jpg"
+  },
+  {
+    "revision": "9df84ad34caad422c4ba20b169c0a02d",
+    "url": "/My_Portfolio/static/media/bgTwo.9df84ad3.jpg"
+  },
+  {
+    "revision": "a384e2d9a4a3cb45b391740c443f9d73",
+    "url": "/My_Portfolio/static/media/house.a384e2d9.png"
+  },
+  {
+    "revision": "a864228d861413ce700766bad05e2c89",
+    "url": "/My_Portfolio/static/media/img.a864228d.jpg"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/My_Portfolio/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "7c376bf0ad1e467bf16ba671c2d199b3",
+    "url": "/My_Portfolio/static/media/logo.7c376bf0.png"
+  },
+  {
+    "revision": "d3a2f53266b220f442bf2d33aa113875",
+    "url": "/My_Portfolio/static/media/me.d3a2f532.png"
+  },
+  {
+    "revision": "15f93ade5747b8dea562cb7276fe2722",
+    "url": "/My_Portfolio/static/media/peeps.15f93ade.png"
+  },
+  {
+    "revision": "dac08c46c06f62bbe1332839ab6d36d5",
+    "url": "/My_Portfolio/static/media/todo.dac08c46.png"
+  }
+]);
